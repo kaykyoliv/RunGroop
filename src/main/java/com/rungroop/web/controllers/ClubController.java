0b9eebc78ab4a4817e2aa11c2bb2f7bef4d8f50c
Kayky Oliveira <kaykydeeoliveira@gmail.com>
2025-01-30 18:04:15 -0300

    Controller + List View

---
+++ src/main/java/com/rungroop/web/controllers/ClubController.java
@@ -0,0 +1,27 @@
+package com.rungroop.web.controllers;
+
+import com.rungroop.web.dto.ClubDTO;
+import com.rungroop.web.services.ClubService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ClubController {
+    private ClubService clubService;
+
+    @Autowired
+    public ClubController(ClubService clubService) {
+        this.clubService = clubService;
+    }
+
+    @GetMapping("/clubs")
+    public String listClubs(Model model) {
+        List<ClubDTO> clubs = clubService.findAllClubs();
+        model.addAttribute("clubs", clubs);
+        return "clubs-list";
+    }
+}
--- src/main/java/com/rungroop/web/repositories/ClubRepository.java
+++ src/main/java/com/rungroop/web/repositories/ClubRepository.java
@@ -2,8 +2,11 @@ package com.rungroop.web.repositories;
 
 import com.rungroop.web.models.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+
+@Repository
 public interface ClubRepository extends JpaRepository<Club, Long> {
     Optional<Club> findByTitle(String url);
 }
